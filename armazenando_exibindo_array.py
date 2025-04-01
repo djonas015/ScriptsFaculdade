2b49964f91c45dc23cd0922d3d9672802450a85a
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-01 10:14:19 -0300

    armazenando e exibindo array

--- armazenando_exibindo_array.py
+++ armazenando_exibindo_array.py
@@ -4,4 +4,4 @@ for i in range(5):
     numero = int(input(f"Digite o {i + 1}° número inteiro: "))
     numeros.append(numero)
 
-print("Os números digitados são: ", numeros)
+print("Os números digitados no array são: ", numeros)
