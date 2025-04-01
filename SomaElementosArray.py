7eff5eece305ceebbcb47379c62c37f308eecaba
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-01 10:07:31 -0300

    Soma dos elementos do array

---
+++ SomaElementosArray.py
@@ -0,0 +1,13 @@
+numeros = []
+
+for i in range(10):
+    numero = int(input(f"Insira o {i + 1}° número inteiro "))
+    numeros.append(numero)
+
+
+def cl_soma(lista):
+    return sum(lista)
+
+
+resultado = cl_soma(numeros)
+print(f"A soma de todos os números do array é: {resultado}")
