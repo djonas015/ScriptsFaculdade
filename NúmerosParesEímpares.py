5655d980dba0c60965ddd815899d2a3245bbf085
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-02 10:38:51 -0300

    Update NúmerosParesEímpares.py

--- NúmerosParesEímpares.py
+++ NúmerosParesEímpares.py
@@ -1,10 +1,12 @@
 numeros = []
 
-print("Digite 10 números inteiros e exibiremos somente os pares.")
+print("Digite 10 números inteiros e exibiremos os pares e ímpares.")
 
 for i in range(10):
     numero = int(input(f"Digite o {i + 1}° número inteiro: "))
     numeros.append(numero)
 
 pares = [x for x in numeros if x % 2 == 0]
+impares = [x for x in numeros if x % 2 != 0]
 print(f"Os números pares digitados foram:{pares}")
+print(f"O números ímpares digitados são:{impares}")
