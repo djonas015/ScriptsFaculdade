ee4dfdde7886a61639211d32541fca202b70be5b
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-02 09:58:48 -0300

    Update MédiaDosValores.py

--- MédiaDosValores.py
+++ MédiaDosValores.py
@@ -4,6 +4,6 @@ print("Digite 6 números inteiros para obter a média dos valores.")
 for i in range(6):
     numero = int(input(f"Digite o {i + 1}° número inteiro: "))
     numeros.append(numero)
-    media = sum(numeros) / len(numeros)
 
+media = sum(numeros) / len(numeros)
 print(f"A média dos valores digitados é: {media:.2f}")
