fd64305248b9701b41f55de75a21549a5e907e76
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-03 12:16:22 -0300

    Create IdentificandoUmNumeroNoArray.py

---
+++ IdentificandoUmNumeroNoArray.py
@@ -0,0 +1,14 @@
+n1 = []
+
+print("Insira 7 números inteiros, depois solicite um número e verificaremos se"
+      " ele se encontra no array")
+for i in range(7):
+    num = int(input(f"Insira o {i + 1}° número inteiro: "))
+    n1.append(num)
+
+num_extra = int(input("Insira um número para sua verificação."))
+verific = num_extra in n1
+if verific == True:
+    print(f"O número {num_extra} foi localizado no array!")
+else:
+    print(f"O número {num_extra} não foi encontrado no array.")
