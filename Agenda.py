e020da9dab6e9bd718f304c373efb000b7688b19
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-11 11:00:33 -0300

    Create Agenda.py

---
+++ Agenda.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+conn = sqlite3.connect("agenda.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS contatos (
+    id INTENGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    telefone TEXT NOT NULL,
+    email TEXT
+)
+""")
+
+
+conn.commit()
+
+
+def add_contato():
+    nome = input("Nome: ")
+    telefone = input("Telefone: ")
+    email = input("Email: ")
+
+
+cursor.execute("INSERT INTO contatos (nome, telefone, email) VALUES (?, ?, ?)", (nome, telefone, email))
+conn.commit()
+print("Contato adicionado com sucesso! \n")
+
+
+def menu():
+    while True:
+        print("=== AGENDA DE CONTATOS ===")
+        print("1. Adicionar contato")
+        print("2. Sair")
+
+        opcao = input("Escolha uma opção: ")      
+        if opcao == "1":
+            add_contato()
+        elif opcao == "2":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida! Tente novamente. \n")
+
+
+menu()
+
+conn.close()
