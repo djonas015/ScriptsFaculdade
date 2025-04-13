9a8eb9397215c493d02fad33adbbd9b24d7cc417
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-13 08:01:15 -0300

    Create RealConvertDolar.py

---
+++ RealConvertDolar.py
@@ -0,0 +1,4 @@
+print("Converta a moeda Real 'BRL R$' para Dólar 'USD $'")
+real = float(input("Valor em 'BRL R$': "))
+dolar = real / 5.87
+print(f"O valor de R${real} convertido em Dólar é: USD ${dolar:.2f}")
