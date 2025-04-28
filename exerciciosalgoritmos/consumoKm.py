36c70bda712baf8edd75d42d74dfce65fe6cd521
Jonas Fernandes <diasjonas121@gmail.com>
2025-04-28 10:37:35 -0300

    Create consumoKm.py

---
+++ exerciciosalgoritmos/consumoKm.py
@@ -0,0 +1,4 @@
+km = float(input("Quilomêtros rodados: "))
+consumo = float(input("Combustivel comsumido: "))
+consumomedio = km / consumo
+print(f"O consumo médio do carro é km/l {consumomedio:.2f}")
