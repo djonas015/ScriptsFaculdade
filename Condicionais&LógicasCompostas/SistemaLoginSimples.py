b079f782f0048b2d1b49ef427b80a97cc86a74ad
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-15 19:39:41 -0300

    Create SistemaLoginSimples.py

---
+++ Condicionais&LógicasCompostas/SistemaLoginSimples.py
@@ -0,0 +1,8 @@
+loginuser = 'usuario@2025'
+usersenha = '2025*'
+login = input("Login: ")
+senha = input("Senha: ")
+if (login, senha) == (loginuser, usersenha):
+    print("Login e senha corretos!")
+else:
+    print("Login ou senha incorretos")
