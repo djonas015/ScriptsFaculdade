fad4a2ff3e1fa6711db0701770f0e62e63f5b947
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-18 17:37:41 -0300

    Update relogio.py

--- MiniProjetoRelogio/relogio.py
+++ MiniProjetoRelogio/relogio.py
@@ -1,18 +1,18 @@
 import time
 
 seg = 0
-min = 0
+mins = 0
 horas = 0
 print("RELÓGIO:")
 while True:
-    print(f"{horas:02}:{min:02}:{seg:02}")
+    print(f"{horas:02}:{mins:02}:{seg:02}")
     time.sleep(1)
     seg += 1
     if seg == 60:
         seg = 0
-        min += 1
-        if min == 60:
-            min = 0
+        mins += 1
+        if mins == 60:
+            mins = 0
             horas += 1
             if horas == 24:
                 break
