924af0a9ca6d8296bfe1147d01fc7f8452c27d50
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-19 11:18:08 -0300

    Update SimulandoCaixaEletronico.py

--- Condicionais&LógicasCompostas/SimulandoCaixaEletronico.py
+++ Condicionais&LógicasCompostas/SimulandoCaixaEletronico.py
@@ -1,5 +1,6 @@
 caixa = 10500
-print(f"Extrato Bancario: R${float(caixa):,.2f}")
+print(f"\nSaldo Bancario: R${float(caixa):,.2f}")
+print('=' * 30)
 while caixa > 0:
     opcao = input("Deseja: 1.Sacar / 2.Depositar : ")
     if opcao == '1':
@@ -8,13 +9,16 @@ while caixa > 0:
             print("Saldo indisponivel.")
         else:
             caixa -= valorsaq
-            print(f"\nExtrato: \nR${caixa:,.2f}")
+            print(f"\nSaldo: \nR${caixa:,.2f}")
+            print('=' * 30)
     elif opcao == '2':
         valordeposito = float(input("Insira o valor do depósito: R$"))
         caixa += valordeposito
-        print(f"\nExtrato: \nR${caixa:,.2f}")
+        print(f"\nSaldo: \nR${caixa:,.2f}")
+        print('=' * 30)
     else:
         print("Opção inválida.")
-    atividade = input("Deseja Encerrar sua atividade? (S / N)").lower()
+    atividade = input("Deseja Encerrar sua atividade? (S / N): ").lower()
     if atividade == 's':
+        print("Sessão Encerrada. Obrigado! Até logo!")
         break
