d6865aa730dad9a7494f78a3b3af84c0bd22e403
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-21 12:14:52 -0300

    Update ClassificandoNotaAaF.py

--- Condicionais&LógicasCompostas/ClassificandoNotaAaF.py
+++ Condicionais&LógicasCompostas/ClassificandoNotaAaF.py
@@ -5,9 +5,9 @@ elif nota <= 5:
     print("Nota E")
 elif nota <= 7:
     print("Nota D")
-elif nota == 8:
+elif nota <= 8:
     print("Nota C")
-elif nota == 9:
+elif nota <= 9:
     print("Nota B")
-elif nota == 10:
+elif nota > 9:
     print("Nota A")
