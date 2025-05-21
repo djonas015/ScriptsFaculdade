b7b93888189a8dcb7f0dcdc026bcdad186b166c0
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-21 12:11:32 -0300

    Create ClassificandoNotaAaF.py

---
+++ Condicionais&LógicasCompostas/ClassificandoNotaAaF.py
@@ -0,0 +1,13 @@
+nota = float(input("Insira a nota: "))
+if nota <= 3:
+    print("Nota F")
+elif nota <= 5:
+    print("Nota E")
+elif nota <= 7:
+    print("Nota D")
+elif nota == 8:
+    print("Nota C")
+elif nota == 9:
+    print("Nota B")
+elif nota == 10:
+    print("Nota A")
