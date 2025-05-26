5728851fa7282da2588bd0ced1389fef05688e30
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-26 08:00:40 -0300

    Create SimulandoTesteMultiplaEscolha.py

---
+++ Condicionais&LógicasCompostas/SimulandoTesteMultiplaEscolha.py
@@ -0,0 +1,65 @@
+print("Em que ano ocorreu a Proclamação da República no Brasil?")
+escolha = input("A) 1808\nB) 1822\nC) 1888\nD) 1889\nE) 1891\nInsira a resposta: ").upper()
+match escolha:
+    case 'A':
+        print("Questão Incorreta")
+    case 'B':
+        print("Questão Incorreta")
+    case 'C':
+        print("Questão Incorreta")
+    case 'D':
+        print("Questão Correta!!")
+    case 'E':
+        print("Questão Incorreta")
+print("\nQuem foi o líder da Inconfidência Mineira, movimento que ocorreu no século XVIII?")
+escolha = input("A) Dom Pedro I\nB) Tiradentes\nC) Duque de Caxias\nD) Getúlio Vargas\nE) Zumbi dos Palmares\nInsira a resposta: ").upper()
+match escolha:
+    case 'A':
+        print("Questão Incorreta")
+    case 'B':
+        print("Questão Correta!!")
+    case 'C':
+        print("Questão Incorreta")
+    case 'D':
+        print("Questão Incorreta")
+    case 'E':
+        print("Questão Incorreta")
+print("\nQual país colonizou o Brasil?")
+escolha = input("A) Espanha\nB) Inglaterra\nC) França\nD) Holanda\nE) Portugal\nInsira a resposta: ").upper()
+match escolha:
+    case 'A':
+        print("Questão Incorreta")
+    case 'B':
+        print("Questão Incorreta")
+    case 'C':
+        print("Questão Incorreta")
+    case 'D':
+        print("Questão Incorreta")
+    case 'E':
+        print("Questão Correta!!")
+print("\nQual foi o presidente do Brasil durante a criação do Plano Real?")
+escolha = input("A) Itamar Franco\nB) Fernando Collor\nC) Fernando Henrique Cardoso\nD) José Sarney\nE) Lula\nInsira a resposta: ").upper()
+match escolha:
+    case 'A':
+        print("Questão Correta!!")
+    case 'B':
+        print("Questão Incorreta")
+    case 'C':
+        print("Questão Incorreta")
+    case 'D':
+        print("Questão Incorreta")
+    case 'E':
+        print("Questão Incorreta")
+print("\nEm que ano a escravidão foi oficialmente abolida no Brasil?")
+escolha = input("A) 1822\nB) 1888\nC) 1889\nD) 1870\nE) 1865\nInsira a resposta: ").upper()
+match escolha:
+    case 'A':
+        print("Questão Incorreta")
+    case 'B':
+        print("Questão Correta!!")
+    case 'C':
+        print("Questão Incorreta")
+    case 'D':
+        print("Questão Incorreta")
+    case 'E':
+        print("Questão Incorreta")
