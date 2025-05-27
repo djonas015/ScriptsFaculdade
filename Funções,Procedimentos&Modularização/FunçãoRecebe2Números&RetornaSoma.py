ba93431b420565b8945ec7fb67901c5aec10a67d
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-27 08:37:17 -0300

    Create FunçãoRecebe2Números&RetornaSoma.py

---
+++ Funções,Procedimentos&Modularização/FunçãoRecebe2Números&RetornaSoma.py
@@ -0,0 +1,10 @@
+n1 = int(input("Insira um número: "))
+n2 = int(input("Insira outro número: "))
+
+
+def soma(n1, n2):
+    resultado = n1 + n2
+    return resultado
+
+
+print(f"Resultado: {soma(n1, n2)}")
