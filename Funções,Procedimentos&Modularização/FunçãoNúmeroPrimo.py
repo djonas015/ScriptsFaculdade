f9310ae085bc2e927d7afe2784fcded58703c570
Jonas Fernandes <diasjonas121@gmail.com>
2025-05-28 07:28:06 -0300

    Update FunçãoNúmeroPrimo.py

--- Funções,Procedimentos&Modularização/FunçãoNúmeroPrimo.py
+++ Funções,Procedimentos&Modularização/FunçãoNúmeroPrimo.py
@@ -1,1 +1,16 @@
 n = int(input("Insira n: "))
+
+
+def primo(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+
+print(primo(n))
