57cfe81ab0ccbfeb3597d1619ccf1e72bc33cdb3
Jonas Fernandes <diasjonas121@gmail.com>
2025-06-10 08:03:23 -0300

    Create FunçãoValidandoCPF.py

---
+++ Funções,Procedimentos&Modularização/FunçãoValidandoCPF.py
@@ -0,0 +1,41 @@
+def validador_cpf():
+    cpf_user = input("Insira o cpf: ")
+    formatacao = ''.join(filter(str.isdigit, cpf_user))
+    if len(formatacao) == 11:
+        if all(digito == formatacao[0] for digito in formatacao):
+            print("CPF inválido: todos os digitos são iguais.")
+            exit()
+        else:
+            cpf_digitos = formatacao[:9]
+            peso = 10
+            soma = 0
+            for digitos in cpf_digitos:
+                num = int(digitos)
+                soma += num * peso
+                peso -= 1
+            resultado = (soma * 10) % 11
+            if resultado >= 10:
+                verif1 = 0
+            else:
+                verif1 = resultado
+            cpf_digitos += str(verif1)
+            peso = 11
+            soma = 0
+            for digitos in cpf_digitos:
+                num = int(digitos)
+                soma += num * peso
+                peso -= 1
+            resultado(soma * 10) % 11
+            if resultado == 10:
+                verif2 = 0
+            else:
+                verif2 = resultado
+            if int(formatacao[9]) == verif1 and int(formatacao[10]) == verif2:
+                print("CPF válido!")
+            else:
+                print("CPF inválido")
+    else:
+        print("CPF inválido: deve conter exatamente 11 dígitos.")
+
+
+validador_cpf()
